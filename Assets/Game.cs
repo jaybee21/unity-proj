e2d4fb566bf10dae56027caa4a809edd94f586d2
Jabule Simanga <43923526+jaybee21@users.noreply.github.com>
2023-02-07 19:32:31 +0200

    Update Game.cs

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -5,7 +5,7 @@ public class Game : MonoBehaviour {
 	
 	public GameObject prefab;
 
-	//  for initialization
+	// code  for initialization
 	void Start () {
 			Instantiate(prefab, new Vector3(50, 2, 50), Quaternion.identity);
 	}
